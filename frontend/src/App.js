117eece5f3f46b48db8f86d30941ee50e27e6617
mlgmontage <kettleofrassel1@gmail.com>
2020-06-11 11:21:11 +0500

    pages && routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+// pages
+import Home from "./pages/home";
+import Names from "./pages/names";
+import Todos from "./pages/todos";
+
+import MainNavigation from "./components/MainNavigation";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Learn React</header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <MainNavigation />
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/names" component={Names} />
+          <Route path="/todos" component={Todos} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/names.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Names extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Names</h1>
+      </div>
+    );
+  }
+}
+
+export default Names;
