33bce35541ad90f60bc950f533e5a20ea757ba67
mlgmontage <kettleofrassel1@gmail.com>
2020-07-13 23:43:36 +0500

    todos rest api

--- backend/api/routes/todos.js
+++ backend/api/routes/todos.js
@@ -3,8 +3,52 @@ const router = Router();
 const Todos = require("../models/Todos");
 
 router.get("/", async (req, res) => {
+  const todos = await Todos.find({});
+  res.json(todos);
+});
+
+router.post("/", async (req, res) => {
+  const { task, isCompleted = false } = req.body;
+
+  const inserted = await Todos.insert({
+    task,
+    isCompleted,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  });
+
+  res.json(inserted);
+});
+
+router.put("/:_id", async (req, res) => {
+  const { _id } = req.params;
+
+  const { task, isCompleted } = req.body;
+
+  const { createdAt } = await Todos.findOne({ _id });
+
+  const numberOfUpdated = await Todos.update(
+    { _id },
+    {
+      task,
+      isCompleted,
+      updatedAt: new Date(),
+      createdAt,
+    },
+    {}
+  );
+
   res.json({
-    message: "Hey",
+    numberOfUpdated,
+  });
+});
+
+router.delete("/:_id", async (req, res) => {
+  const { _id } = req.params;
+
+  const numberOfDeleted = await Todos.remove({ _id });
+  res.json({
+    numberOfDeleted,
   });
 });
 
