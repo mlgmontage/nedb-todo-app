23b7b27aafdf623c334c7678fa21a924c39f5e97
mlgmontage <kettleofrassel1@gmail.com>
2020-07-14 09:53:13 +0500

    setup axios && bootstrap

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "../node_modules/bootswatch/dist/journal/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
